59d3882c48dec1d397ec1ee451a2b313706cb318
logicp <logicp@logicp.ca>
2021-11-07 12:21:35 -0500

    namespacing

--- file_iterator.hpp
+++ file_iterator.hpp
@@ -5,7 +5,10 @@
 #include <string>
 #include <vector>
 
-static std::vector<uint8_t> readFileAsBytes(const std::string& file_path)
+namespace Kiqoder
+{
+
+static std::vector<uint8_t> ReadFileAsBytes(const std::string& file_path)
 {
   std::ifstream        stream(file_path, std::ios::binary);
   std::vector<uint8_t> bytes{std::istreambuf_iterator<char>(stream), {}};
@@ -13,14 +16,28 @@ static std::vector<uint8_t> readFileAsBytes(const std::string& file_path)
   return bytes;
 }
 
-
 static const uint32_t PACKET_SIZE{4096};
+template <typename T>
 class FileIterator
 {
+static const uint32_t HEADER_SIZE{4};
 public:
 FileIterator(const std::string& path)
-: m_buffer(readFileAsBytes(path)),
-  data_ptr(nullptr)
+: m_buffer(PrepareBuffer(std::move(ReadFileAsBytes(path)))),
+  data_ptr(nullptr),
+  m_bytes_read(0)
+{
+  if (m_buffer.size())
+  {
+    data_ptr = m_buffer.data();
+    m_size   = m_buffer.size();
+  }
+}
+
+FileIterator(const T* bytes, const size_t size)
+: m_buffer(PrepareBuffer(std::move(std::vector<T>(bytes, bytes + size)))),
+  data_ptr(nullptr),
+  m_bytes_read(0)
 {
   if (!m_buffer.empty())
   {
@@ -29,43 +46,83 @@ FileIterator(const std::string& path)
   }
 }
 
+
+FileIterator(const FileIterator& i)
+: m_buffer(std::move(i.m_buffer)),
+  data_ptr(std::move(i.data_ptr)),
+  m_bytes_read(std::move(i.m_bytes_read)),
+  m_size(std::move(i.m_size))
+{}
+
+FileIterator(FileIterator&& i)
+: m_buffer(std::move(i.m_buffer)),
+  data_ptr(std::move(i.data_ptr)),
+  m_bytes_read(std::move(i.m_bytes_read)),
+  m_size(std::move(i.m_size))
+{}
+
+static std::vector<T> PrepareBuffer (std::vector<T>&& data)
+{
+  const uint32_t bytes = (data.size() + HEADER_SIZE);
+  std::vector<T> buffer{};
+  buffer.reserve(bytes);
+  buffer.emplace_back((bytes >> 24) & 0xFF);
+  buffer.emplace_back((bytes >> 16) & 0xFF);
+  buffer.emplace_back((bytes >> 8 ) & 0xFF);
+  buffer.emplace_back((bytes      ) & 0xFF);
+  buffer.insert(buffer.end(), std::make_move_iterator(data.begin()), std::make_move_iterator(data.end()));
+  return buffer;
+};
 struct PacketWrapper
 {
-  PacketWrapper(uint8_t* ptr_, uint32_t size_)
-  : ptr(ptr_),
-    size(size_) {}
-  uint8_t* ptr;
-  uint32_t size;
+PacketWrapper(T* ptr_, uint32_t size_)
+: ptr(ptr_),
+  size(size_) {}
 
-  uint8_t* data() { return ptr; }
+T* data() { return ptr; }
+
+T*       ptr;
+uint32_t size;
 };
 
 bool has_data()
 {
-  return data_ptr != nullptr;
+  return (data_ptr != nullptr);
+}
+
+std::string to_string()
+{
+  std::string data_s{};
+  for (uint32_t i = 0; i < m_buffer.size(); i++) data_s += std::to_string(+(*(m_buffer.data() + i)));
+  return data_s;
 }
 
 PacketWrapper next() {
-  uint8_t* ptr = data_ptr;
+  uint32_t size;
   uint32_t bytes_remaining = m_size - m_bytes_read;
-  uint32_t size{};
+  T*       ptr             = data_ptr;
+
   if (bytes_remaining < PACKET_SIZE)
   {
-    size = bytes_remaining;
+    size     = bytes_remaining;
     data_ptr = nullptr;
   }
   else
   {
-    size      = PACKET_SIZE;
+    size     =  PACKET_SIZE;
     data_ptr += PACKET_SIZE;
   }
-    return PacketWrapper{ptr, size};
+
+  m_bytes_read += size;
+
+  return PacketWrapper{ptr, size};
 }
 
 private:
 
-std::vector<uint8_t> m_buffer;
-uint8_t*             data_ptr;
-uint32_t             m_bytes_read;
-uint32_t             m_size;
+std::vector<T> m_buffer;
+T*             data_ptr;
+uint32_t       m_bytes_read;
+uint32_t       m_size;
 };
+} // ns kiqoder
